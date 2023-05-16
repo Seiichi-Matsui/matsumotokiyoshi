d0f9c9064c2c1beae30c898d7fc5436f0c6fd97d
Seiichi-Matsui <s-matsui@zima-corp.com>
2023-05-16 16:22:50 +0900

    navの余白調整とpagetopボタン実装

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,4 +1,6 @@
 import './css-vars.js';
 import "../../../node_modules/simplebar/dist/simplebar.min.js";
+import "../../../node_modules/smoothscroll/smoothscroll.min.js";
 // import './components/fix-nav-button.js';
 import './components/nav-filter.js';
+import './components/scrollTop.js';
