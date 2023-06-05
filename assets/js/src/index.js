610b1bd0df58defd1fab71ab040dc4ba2266249d
Seiichi-Matsui <s-matsui@zima-corp.com>
2023-06-05 21:07:35 +0900

    スリム化

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,6 +1,2 @@
-import './css-vars.js';
-import "../../../node_modules/simplebar/dist/simplebar.min.js";
-import "../../../node_modules/smoothscroll/smoothscroll.min.js";
-// import './components/fix-nav-button.js';
 import './components/nav-filter.js';
 import './components/scrollTop.js';
